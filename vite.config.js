599ec0f25591a9b6ed7102b5a38419a5a9c88f1f
Emirhan Erol <erolemirhan42@gmail.com>
2025-12-14 04:33:52 +0300

    deployy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  },
   server: {
     host: '0.0.0.0', // Tüm ağ arayüzlerinde dinle
     port: 3000,
